17237054cac4c880b67efaf75ff73431ba427a8a
lifang.yang <lifang.yang@qiaofang.local>
2019-03-26 21:26:34 +0800

    ïnit project

---
+++ week1/chass_0302/chass_baseinfo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-03-02 21:52
+# @Author  : yang_cube
+# @Site    : 
+# @File    : chass_baseinfo.py
+# @Software: PyCharm
+#基础语法
+
+
+
+"""print(1)
+print(2)
+print(3)"""
+
+a='hsdah'\
+  'hadu'
+print(a)
+b='你好，我的名字是："杨"'
+print(b)
+
+c=''''nihao,
+       wohis 
+       haushudasjf'''
+print(c)
+
+#占位符
+name='yang'
+age=22
+salary=9000.9
+print('我的名字是%s,我的年龄是%d,我的薪水是%.2f'%(name,age,salary))
+print('我的名字是{},我的年龄是{},我的薪水是{}'.format(name,age,salary))
+print('我的名字是{1},我的年龄是{1},我的薪水是{1}'.format(name,age,salary))
+
+
---
+++ week1/chass_0302/class_9.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-03-18 21:20
+# @Author  : yang_cube
+# @Site    : 
+# @File    : class_9.py
+# @Software: PyCharm
+# def add(*a):
+# #     print('数据类型：', a)
+# #     print('数据类型：', *a)
+# # t=((1,2),(3,4))
+# # add(t)
+for i in (1,2,3,4,5):
+    print(' '*(5-i),end=' ')
+    print(' *'*i)
