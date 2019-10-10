acfa0f3fb4efd615fb4f0c67c7cf58e30b10dd6b
lifang.yang <lifang.yang@qiaofang.local>
2019-10-10 20:18:03 +0800

    Merge remote-tracking branch 'remotes/origin/test'
    # Conflicts:
    #	README.md

---
+++ week2/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-03-27 21:06
+# @Author  : yang_cube
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ week2/class_0327.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019-03-27 21:07
+# @Author  : yang_cube
+# @Site    : 
+# @File    : class_0327.py
+# @Software: PyCharm
+s='pythonyy'
+# res=s.replace('y')
+res=s.split('y',2)
+print(res)
